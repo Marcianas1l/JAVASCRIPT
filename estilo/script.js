f39113935f7304816ce4ac01135ffc362a0f5bc7
Marciana Silva <32343588+Marcianas1l@users.noreply.github.com>
2020-06-23 21:48:59 -0300

    Update script.js

--- estilo/script.js
+++ estilo/script.js
@@ -1,43 +1,24 @@
-function verificar() {
-    console.log('entrou');
-    
-    var nascimento = window.document.getElementById('nascimento');
-    var ano = nascimento.value;
-    var anoAtual = new Date().getFullYear();
-    var idade = anoAtual - ano;
+function carregar() {
 
-    var sexo = document.querySelector('input[name="selecao"]:checked').value;
-    var frase = document.getElementById('msg');          
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var dia = hora + ' horas e ' + minuto + ' minutos';
 
-    var imagem = document.querySelector('img#foto');
+    var msg = window.document.getElementById('texto');
+    msg.innerHTML = `<p>Agora são ${dia}</p>`
 
-    if ((idade>=1 && idade <=12) && (sexo == "Masculino")) {
-        frase.innerHTML = `Verificamos que você é um menino e tem ${idade} Anos`;
-        imagem.src = "./imagens/crianca-homem.jpg";
-    } else if ((idade >= 1 && idade <= 12) && (sexo == "Feminino")) {
-        frase.innerHTML = `Verificamos que você é uma menina e tem ${idade} Anos`;
-        imagem.src = "./imagens/crianca-mulher.jpg";
-    } else if ((idade >= 13 && idade <=17) && (sexo == "Masculino")) {
-        frase.innerHTML = `Verificamos que você é um adolescente e tem ${idade} Anos`;
-        imagem.src = "./imagens/homem-adolescente.jpg";
-    } else if ((idade >= 13 && idade <= 17) && (sexo == "Feminino")) {
-        frase.innerHTML = `verificamos que você é uma adolescente e tem ${idade} Anos`;
-        imagem.src = "./imagens/mulher-adolescente.jpg";
-    } else if ((idade >= 18 && idade <= 21) && (sexo == "Masculino")) {
-        frase.innerHTML = `Verificamos que você é um homem e tem ${idade} Anos`;
-        imagem.src = "./imagens/homem-adulto.jpg"
-    } else if ((idade >= 18 && idade <= 21) && (sexo == "Feminino")) {
-        frase.innerHTML = `Vericamos que você é uma homem e tem ${idade} Anos`;
-        imagem.src = "./imagens/mulher-adulta.jpg";
-    } else if ((idade >= 22 && idade <= 110) && (sexo == "Masculino")) {
-        frase.innerHTML = `Verificamos que você é um homem e tem ${idade} Anos`;
-        imagem.src = "./imagens/homem-idoso.jpg";
-    } else if ((idade >= 22 && idade <= 110) && (sexo == "Feminino")) {
-        frase.innerHTML = `Verificamos que você é uma mulher e tem ${idade} Anos`;
-        imagem.src = "./imagens/mulher-idosa.jpg"
+    var img = window.document.getElementById('imagem');
+
+    if (hora >= 6 && hora < 12) {
+        img.src = "../imagens/manha.jpg"
+    } else if (hora >= 12 && hora <= 17) {
+        img.src = "../imagens/tarde.jpg"
+    } else if (hora >= 18 && hora <= 24) {
+        img.src = "../imagens/noite.jpg"
     } else {
-        frase.innerHTML = `sua idade é:  ${idade} Anos`;
-        imagem.src = ".imagens/nana.jpg";
+        img.src = "../imagens/madrugada.jpg"
+        document.body.style.background = '#000';
+        document.body.style.color = "#fff";
     }
-
 }
